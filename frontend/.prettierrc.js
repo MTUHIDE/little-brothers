94d40772f050a6335afa9c64b62849d347669a2e
Ethan <ethan@etekweb.net>
2021-10-06 23:53:03 -0400

    set formatter configuration

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  printWidth: 120,
+  proseWrap: 'never',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+};
