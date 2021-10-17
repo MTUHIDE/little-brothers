00862df0991372da77e64211f869e29ddd67755f
mdboisve <59846393+mdboisve@users.noreply.github.com>
2021-10-17 13:49:43 -0400

    Added calendar in Home.vue, added logo in assets

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <img alt="Little Brothers MI logo" src="../assets/LB_logo.png" width="50%" height="50%"/>
+    <h1>Welcome User!</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!-- <div style="background-color: blue"> -->
+      <b-row style="background-color:white"> <!--  the arrow nav bar right above the calendar -->
+    <b-col >
+      <b-calendar block v-model="value" @context="onContext" locale="en-US" ></b-calendar>
+    </b-col>
+
+    <!-- stores all of the information for the date that was clicked -->
+    <!-- <b-col>
+      <p>Value: <b>'{{ value }}'</b></p>
+      <p class="mb-0">Context:</p> -->
+      <!-- <pre class="small">{{ context }}</pre>
+    </b-col> --> -->
+    </b-row>
+    </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    data() {
+      return {
+        value: '',
+        context: null
+      }
+    },
+    methods: {
+      onContext(ctx) {
+        this.context = ctx
+      }
+    }
+  }
+
 </script>
