7c875b6a460f2668b1e6c4df487d3a2d0a0e084e
TomAllenBerg <tomberg@mtu.edu>
2021-10-18 19:54:29 -0400

    Add login view and component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Content from "../views/Content.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/horses",
     name: "Content",
     component: Content,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
     path: "/about",
