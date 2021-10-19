beb0582ecadbc30c462ad41b30232e2691b9c1cd
Ethan <ethan@etekweb.net>
2021-10-18 20:13:00 -0400

    add link to driver

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,9 @@
       <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
       <router-link to="/horses">Horses</router-link>
     </div> -->
-    <DriverNavbar v-if="$route.meta.driver" />
     <router-link to="/login">Login</router-link>
+    <router-link to="/driver">Driver</router-link>
+    <DriverNavbar v-if="$route.meta.driver" />
     <router-view />
     <div class="footer">Brought to you by HIDE (MTU)</div>
   </div>
