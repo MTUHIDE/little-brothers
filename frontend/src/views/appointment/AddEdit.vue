ab56f2a62655f9dade28a594b06061868a90a5ed
Nicholas Zidel <nicholaszidel@gmail.com>
2021-11-04 19:46:33 -0400

    Continued work on AddEdit page.

--- frontend/src/views/appointment/AddEdit.vue
+++ frontend/src/views/appointment/AddEdit.vue
@@ -1,5 +1,6 @@
 <template>
     <b-container>
+        <!-- Row with back, header and save  -->
         <b-row>
             <b-col>
                 <button>Back</button>
@@ -11,27 +12,72 @@
                 <button>Save</button>
             </b-col>
         </b-row>
+
+        <!-- Row for form and client information columns -->
         <b-row>
             <b-col>
-                <table>
-                    <tr>
-                        <th>Client Name</th>
-                        <th>Driver</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        
-                    </tr>
-                    <tr>
-                        <th>Pickup Address</th>
-                        <th>Destination Address</th>
-                        <th>Notes</th>
-                    </tr>
-                </table>
-            </b-col>
-            <b-col>
+                <form name="Appointment">
+                    <b-form-row>
+                        <div class="col-25">
+                            <label for="cname">Client Name</label>
+                        </div>
+                        <div class="col-75">
+                            <input id="cname" v-model="clientName">    
+                        </div>
+                    </b-form-row>
+                    <b-form-row>
+                       <div class="col-25">
+                            <label for="driver">Driver</label>
+                        </div>
+                        <div class="col-75">
+                            <select id="driver">
+                            <option value="SelectDriver" disabled=true selected>--Select A Driver--</option>
+                            <option value="Driver 1">Driver One</option>
+                            <option value="Driver 2">Driver Two</option>
+                            </select>
+                        </div>
+                    </b-form-row>
+                    <b-form-row>
+                        <div class="col-25">
+                            <label for="datetime">Date/Time</label>
+                        </div>
+                        <div class="col-75">
+                            <input id="datetime" type="datetime-local" >  
+                        </div>
+                    </b-form-row>
+                    <b-form-row>
+                        <div class="col-25">
+                            <label for="pickup">Pickup Address</label>
+                        </div>
+                        <div class="col-75">
+                            <textarea id="pickup"></textarea>
+                        </div>
+                    </b-form-row>
+                    <b-form-row>
+                        <div class="col-25">
+                            <label for="dropoff">Dropoff Address</label>
+                        </div>
+                        <div class="col-75">
+                            <textarea id="dropoff"></textarea>                    
+                        </div>
+                    </b-form-row>
+                    <b-form-row>
+                        <div class="col-25">
+                            <label for="extranotes">Addititonal Notes</label>
+                        </div>
+                        <div class="col-75">
+                            <textarea id="extranotes"></textarea>
+                        </div>
+                    </b-form-row>
+                </form>
 
             </b-col>
+
+            <b-col>
+                <p>Client Info Here</p>
+            </b-col>
         </b-row>
+
     </b-container>
 </template>
 
@@ -48,4 +94,22 @@
 .row {
   margin-top: 32px;
 }
+label {
+    display: inline-block;
+}
+input, select, textarea {
+    width: 100%;
+    box-sizing: border-box;
+}
+
+.col-25 {
+    float: left;
+    text-align: left;
+    width: 25%;
+}
+.col-75 {
+    float: left;
+    width: 75%;
+}
+
 </style>
