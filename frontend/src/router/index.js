afd3c2a299f9c98ca090453b9c76dac3129c4643
Ethan <ethan@etekweb.net>
2021-12-05 22:40:17 -0500

    added previous updates page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import DriverAbout from '../views/driver/DriverAbout.vue';
 import DriverHome from '../views/driver/DriverHome.vue';
 import DriverCalendar from '../views/driver/DriverCalendar.vue';
 import DriverUpdate from '../views/driver/DriverUpdate.vue';
+import DriverPrevUpdates from '../views/driver/DriverPrevUpdates.vue';
 import DriverMoreInfo from '../views/driver/DriverMoreInfo.vue';
 import Login from '../views/Login.vue';
 import DesktopCalendar from '../views/desktop/DesktopCalendar.vue';
@@ -52,6 +53,12 @@ const routes = [
     path: '/driver/update',
     name: 'DriverUpdate',
     component: DriverUpdate,
+    meta: { driver: true },
+  },
+  {
+    path: '/driver/prev-updates',
+    name: 'DriverPrevUpdates',
+    component: DriverPrevUpdates,
     meta: { driver: true },
   },
   {
