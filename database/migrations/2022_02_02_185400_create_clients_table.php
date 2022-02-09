e50406e50c02616299619f5586a24a6c95276d63
Chanpech <70418825+Chanpech@users.noreply.github.com>
2022-02-09 17:35:42 -0500

    Created the drivers table and client table
    I have went in and created the drivers and client table. Might need to modify the data type later as we work with the front end

--- database/migrations/2022_02_02_185400_create_clients_table.php
+++ database/migrations/2022_02_02_185400_create_clients_table.php
@@ -15,7 +15,15 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->string('client_address', 255);
+            $table->string('client_phone_number', 255);
+            $table->string('mobility', 255);
+            $table->string('client_notes', 255)->nullable();
+            $table->bigIncrement('#ofcancels'); //Will need to talk to members
             $table->timestamps();
+
+            $table->primary('client_id');
         });
     }
 
