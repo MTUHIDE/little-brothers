46a8bc01668539e993c8d290ca0313454ba2fab9
Ben Fosdick <blfosdic@mtu.edu>
2022-03-07 22:54:22 -0500

    Client page update

--- resources/components/busforms/ClientForm.vue
+++ resources/components/busforms/ClientForm.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="client">
     <div class="container-fluid">
-      <!-- Top bar with back and save/edit button -->
 
-      <!-- <Button buttonText="Back" to="\calendar"/>
-        <b-button to="/calendar">Back</b-button> -->
         <h1>Client</h1>
 
-        <div class="button-container">
-          <button type="button" :to="redirect" :style="{ display: activeBack }" class="btn btn-primary">Back</button>
-          <button type="edit" class="btn btn-primary" id="clientEdit">Edit</button>
+        <div class="button-container mb-3">
+          <button type="button" :to="redirect" :style="{ display: activeBack }" class="btn btn-primary px-3">Back</button>
+          <button type="edit" class="btn btn-primary px-3" id="clientEdit">Edit</button>
         </div>
 
         <!-- TODO Update this so that it can be populated with data from the backend-->
-        <div class="card text-start border-dark m-3">
-          <div class="card-header">Name</div>
+        <div class="card text-start border-dark mx-auto w-50" >
+          <div class="card-header text-center fw-bold">Name: Carl Wheezer </div>
           <div class="card-body">
-            <address class="card-text">Address:</address>
-            <p class="card-text">Phone:</p>
-            <p class="card-text">Mobility:</p> 
+            <div class="row">
+              <div class="col-4">
+                <p class="card-text fw-bold">Address: </p>
+                <p class="card-text fw-bold">Phone number:</p>
+                <p class="card-text fw-bold">Mobility:</p> 
+                <p class="card-text fw-bold">Number of cancels:</p> 
+                <p class="card-text fw-bold">Additional notes:</p> 
+              </div>
+              <div class="col">
+                <p class="card-text">TMP 713 Road St Apt 2 </p>
+                <p class="card-text">TMP (906) 333-1542</p>
+                <p class="card-text">TMP Wheelchair</p> 
+                <p class="card-text">TMP 2</p> 
+                <p class="card-text"> * TMP Is partially deaf</p> 
+              </div>
+            </div>
           </div>
         </div>
-      
-
     </div>
   </div>
 </template>
