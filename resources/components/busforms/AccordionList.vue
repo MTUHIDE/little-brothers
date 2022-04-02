66c72907a6f23165d348405462877b2f3bdfe383
Ben Fosdick <blfosdic@mtu.edu>
2022-04-02 15:36:49 -0400

    Client controler updated, cleaned up client form and accordion list.

--- resources/components/busforms/AccordionList.vue
+++ resources/components/busforms/AccordionList.vue
@@ -2,23 +2,12 @@
   
     <div class="accordion-item">
       <h2 class="accordion-header" >
-        <button
-          class="accordion-button collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          v-bind:data-bs-target="'#a'+collapseId"
-          aria-expanded="false"
-          aria-controls="collapseId"
-        >
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" v-bind:data-bs-target="'#collapse'+collapseId" aria-expanded="false" aria-controls="collapseId">
           {{name}}
         </button>
       </h2>
-      <div
-        v-bind:id="'a'+collapseId"
-        class="accordion-collapse collapse"
-        data-bs-parent="#accordionExample"
-      >
-        <div class="accordion-body">{{client_address}}</div>
+      <div v-bind:id="'collapse'+collapseId" class="accordion-collapse collapse" data-bs-parent="#anAccordion">
+        <div class="accordion-body">{{address}}+{{numofcancels}}+{{phone_number}}</div>
       </div>
     </div>
   
@@ -28,10 +17,14 @@
 export default {
   name: "AccordionList",
   props: {
-    headingId: { type: Number, required: true},
     collapseId: { type: Number, required: true},
     name: { type: String, required: true },
-    client_address: { type: String, required: true }
+    address: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    mobility: { type: String, required: true },
+    notes: { type: String, required: false },
+    numofcancels: { type: String, required: true },
+
   }
 };
 </script>
--- resources/components/busforms/ClientForm.vue
+++ resources/components/busforms/ClientForm.vue
@@ -15,30 +15,18 @@
           <button type="button" class="btn btn-primary px-3" id="clientEdit" data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
         </div>
 
-        <div class="accordion" id="accordionTest">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              Accordion Item #1
-              </button>
-            </h2>
-           <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionTest">
-          <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-  </div>
-
-        <div class="accordion" id="accordionExample">
+        <div class="accordion" id="anAccordion">
           <AccordionList 
             v-show="1" 
             v-for="client in clients"
             :key="client.id"
-            :headingId="client.id"
             :collapseId="client.id"
             :name="client.client_name"
-            :client_address="client.client_address"
+            :address="client.client_address" 
+            :phone_number="client.client_phone_number"
+            :mobility="client.mobility"
+            :notes="client.client_notes"
+            :numofcancels="client.number_of_cancels" 
         ></AccordionList>
         </div>
 
