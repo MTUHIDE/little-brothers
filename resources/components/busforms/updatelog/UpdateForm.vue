38b502b621c63950ef7b9c7c2e7ad00a4af27608
unknown <aphamach@mtu.edu>
2022-09-15 17:11:32 -0400

    "New table feature added for the driver log."

---
+++ resources/components/busforms/updatelog/UpdateForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <Table />
+</template>
+
+<script>
+// import HelloWorldVue from "./components/HelloWorld.vue";
+import Table from "../../../components/busforms/updatelog/Table.vue";
+export default {
+  name: "App",
+  components: {
+    Table,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
